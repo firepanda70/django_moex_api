913a1c7e3bfd9c8a3f16929bd5ee0d52e1aecc21
Михаил Кочуров <firepanda70@gmail.com>
2024-01-29 01:10:11 +0500

    bugfix

--- django_moex_api/moex_client/views.py
+++ django_moex_api/moex_client/views.py
@@ -9,13 +9,15 @@ from moex_client import moex_client, MOEXClinetException
 
 @cache_page(TIMEOUT)
 def get_current_usd(request):
-    history = moex_client.get_history()
     try:
         last = moex_client.get_usd_rub_rate()
     except MOEXClinetException:
+        history = moex_client.get_history()
         if len(history) == 0:
             return JsonResponse({'exc': 'Temporaly unavaliable'}, status=500)
         last = history[-1]
+    else:
+        history = moex_client.get_history()
     data = {
         'last': asdict(last),
         'history': [
